2cf7167dd33ea0b58c32f2006b79251b17b7098b
Oleksii Kurilo <alex.kurilo77@gmail.com>
2020-02-28 16:42:19 +0200

    create react project and add react-router-dom, materializecss

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'materialize-css';
+
+
+function App() {
+  return (
+    <div className={'container'}>
+      <h1>
+        My App component!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
