d7830cfb776cc2c7d212abf70543f0d349f74d44
Oleksii Kurilo <alex.kurilo77@gmail.com>
2020-02-28 15:24:38 +0200

    adding login verification, bcryptjs-  and jsonwebtoken-library

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,13 @@
 const {Router} = require('express');
 const config = require("config");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const {check, validationResult} = require('express-validator');
 const User = require('../models/User');
 
 const router = Router();
 const SALT = config.get('salt');
+const JWT_SECRET = config.get('jwtSecret');
 
 //prefix /api/auth
 router.post(
@@ -16,42 +18,91 @@ router.post(
             .isLength({ min: 6 })
     ],
     async (request, response) => {  //route /api/auth/register
-    try {
-        const errors = validationResult(request);
+        try {
+            const errors = validationResult(request);
 
-        if (!errors.isEmpty()) {
-            return response.status(462).json({
-                errors: errors.array(),
-                message: 'Incorrect data during registration.'
+            if (!errors.isEmpty()) {
+                return response.status(462).json({
+                    errors: errors.array(),
+                    message: 'Incorrect data during registration.'
+                });
+            }
+
+            const {email, password} = request.body;
+            const candidate = await User.findOne({
+                email: email
             });
+
+            if (candidate) {
+                return response.status(460).json({message: `Such user exists`});
+            }
+
+            const hashedPassword = await bcrypt.hash(password, SALT);
+            const user = new Usere({
+                email: email,
+                password: hashedPassword,
+                name: name
+            });
+
+            await user.save();
+
+            response.status(201).json({message: `User ${user.name} is created.`});
+        } catch (e) {
+            response.status(500).json({message: `Something went wrong when registration, try it again.`});
         }
-
-        const {email, password} = request.body;
-        const candidate = await User.findOne({
-            email: email
-        });
-
-        if (candidate) {
-            return response.status(460).json({message: `Such user exists`});
-        }
-
-        const hashedPassword = await bcrypt.hash(password, SALT);
-        const user = new Usere({
-            email: email,
-            password: hashedPassword,
-            name: name
-        });
-
-        await user.save();
-
-        response.status(201).json({message: `User ${user.name} is created.`});
-    } catch (e) {
-        res.status(500).json({message: `Something  went wrong when registration, try it again.`})
     }
-});
+);
 
-router.post('/login', async (request, response) => {  //route /api/auth/login
+router.post(
+    '/login',
+    [
+        check('email', 'Enter valid email')
+            .normalizeEmail()
+            .isEmail(),
+        check('password', 'Enter valid password')
+            .exists()
+    ],
+    async (request, response) => {  //route /api/auth/login
+        try {
+            const errors = validationResult(request);
 
-});
+            if (!errors.isEmpty()) {
+                return response.status(463).json({
+                    errors: errors.array(),
+                    message: 'Invalid login data.'
+                });
+            }
+
+            const {email, password, name} = request.body;
+            const user = await User.findOne({
+                email: email
+            });
+
+            if (!user) {
+                return request.status(464).json({ message: `User with such ${email} not found.`});
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);// сравниваю password введенный юзером и хешированный в БД
+
+            if (!isMatch) {
+                return response.status(465).json({ message: `Invalid password ${password}, try again.`});
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                JWT_SECRET,
+                { expiresIn: '1h'}
+            );
+
+            response.json({
+                token,
+                userId: user.id
+            });
+
+        } catch (e) {
+            response.status(500).json({message: `Something went wrong when login, try again.`});
+        }
+    }
+);
 
 module.exports = router;
