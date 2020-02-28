d4a209a512b25d8ee83b9f8ef64fa518bb740503
Oleksii Kurilo <alex.kurilo77@gmail.com>
2020-02-28 14:41:46 +0200

    adding validations user registrations data

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,52 @@
 const {Router} = require('express');
-const router = Router();
-//prefix /api/auth
+const config = require("config");
+const bcrypt = require('bcryptjs');
+const {check, validationResult} = require('express-validator');
+const User = require('../models/User');
 
-router.post('/register', async (request, response) => {  //route /api/auth/register
+const router = Router();
+const SALT = config.get('salt');
+
+//prefix /api/auth
+router.post(
+    '/register',
+    [
+        check('email', 'Invalid e-mail...').isEmail(),
+        check('password', 'Invalid password, minimum password length must be at least 6 characters')
+            .isLength({ min: 6 })
+    ],
+    async (request, response) => {  //route /api/auth/register
     try {
+        const errors = validationResult(request);
+
+        if (!errors.isEmpty()) {
+            return response.status(462).json({
+                errors: errors.array(),
+                message: 'Incorrect data during registration.'
+            });
+        }
+
         const {email, password} = request.body;
+        const candidate = await User.findOne({
+            email: email
+        });
+
+        if (candidate) {
+            return response.status(460).json({message: `Such user exists`});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, SALT);
+        const user = new Usere({
+            email: email,
+            password: hashedPassword,
+            name: name
+        });
+
+        await user.save();
+
+        response.status(201).json({message: `User ${user.name} is created.`});
     } catch (e) {
-        res.status(560).json({message: `Something  went wrong when registration, try it again.`})
+        res.status(500).json({message: `Something  went wrong when registration, try it again.`})
     }
 });
 
