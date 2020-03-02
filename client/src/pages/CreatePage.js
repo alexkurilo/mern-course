42677b4e3ec9318231807d238efca3b8ec25c501
Oleksii Kurilo <alex.kurilo77@gmail.com>
2020-03-02 16:02:48 +0200

    VIP

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const CreatePage = () => {
+    useEffect(() => {
+        window.M.updateTextFields();//work input-fields active
+    }, []);
+    const [link, setLink] = useState('');
+
+    const changeHandler = event => {
+        console.log({ [event.target.name]: event.target.value});
+    };
+
     return (
-        <div>
-            <h1>
-                My Create page.
-            </h1>
+        <div className="row">
+            <div className="col s8 offset-s2 padding_top">
+                <div className="input-field">
+                    <input
+                        placeholder="Enter link"
+                        id="link"
+                        type="text"
+                        name="link"
+                        onChange={changeHandler}
+                    />
+                    <label htmlFor="email">Email</label>
+                </div>
+            </div>
         </div>
     );
 };
